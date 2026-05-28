6dcdac9c0b07b3a1d1397f301d28a704ecbac5f1
dewald <dewald.oosthuizen@rank.com>
2026-05-28 23:30:24 +0200

    fix: resolve issue #10 - BUG: Fix the game to work with godot 4

--- scripts/generic_tween_script.gd
+++ scripts/generic_tween_script.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-var tweenNode # set this node inside the _ready function of child node extending this script
-
 var tweenRunning = false
 @export var moveDirectionX = 0 # -1 = left, 1 = right
 @export var moveDirectionY = 0 # -1 = up, 1 = down
@@ -12,21 +10,17 @@ var movementPosition
 @export var tweenDuration = 4 # duration of tween moving from position
 var trans_type = Tween.TRANS_LINEAR
 var ease_type = Tween.EASE_IN_OUT
+var _active_tween: Tween = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _start_tween_process():
-	if (tweenNode and !tweenRunning && canTween):
+	if (!tweenRunning and canTween):
 		tweenRunning = true
-		
-		# check if the signal is connected
-		if (!tweenNode.is_connected("finished", Callable(self, "_on_tween_completed"))):
-			# connect the signal, once tween is completed it will call the _on_tween_completed method
-			tweenNode.connect("finished", Callable(self, "_on_tween_completed"))
-		
-		# tween properties
-		var tween = tweenNode.tween_property(self, "position", movementPosition, tweenDuration)
-		tween.set_trans(trans_type)
-		tween.set_ease(ease_type)
+		_active_tween = get_tree().create_tween()
+		_active_tween.tween_property(self, "position", movementPosition, tweenDuration) \
+			.set_trans(trans_type) \
+			.set_ease(ease_type)
+		_active_tween.finished.connect(_on_tween_completed)
 
 
 func _set_initial_movement(initPosition):
@@ -50,9 +44,10 @@ func _change_y_direction():
 		moveDirectionY = 1
 
 
-func _on_tween_completed(object, key):
+func _on_tween_completed():
 	_change_x_direction()
 	_change_y_direction()
 	
 	movementPosition = self.position + Vector2(moveDistanceX * moveDirectionX, moveDistanceY * moveDirectionY)
 	tweenRunning = false
+	_active_tween = null
